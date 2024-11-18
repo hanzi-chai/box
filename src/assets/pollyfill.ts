70b13040ea774c6e93df4caae2b56bcb71c0ab28
yb6b <seetxx@qq.com>
2024-11-18 12:16:28 +0800

    fix: cloudflare pages

---
+++ src/assets/pollyfill.ts
@@ -0,0 +1,19 @@
+window.requestIdleCallback =
+    window.requestIdleCallback ||
+    function (cb) {
+        const start = Date.now()
+        return setTimeout(() => {
+            cb({
+                didTimeout: false,
+                timeRemaining() {
+                    return Math.max(0, 50 - (Date.now() - start))
+                },
+            })
+        }, 1)
+    }
+
+window.cancelIdleCallback =
+    window.cancelIdleCallback ||
+    function (id) {
+        clearTimeout(id)
+    }
